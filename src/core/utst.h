4450662d0e8cd6c67fc40f2386a78ff56f06f7fd
lichen-liu <issac.liu@mail.utoronto.ca>
2021-11-22 00:45:00 -0500

    better UTST_ASSERT

--- src/core/utst.h
+++ src/core/utst.h
@@ -65,34 +65,38 @@ namespace UTST
         __utst_test_registry.execute_functions();    \
     }
 
-#define UTST_TEST(test_name)                                                                                                 \
-    static void UTST_PPCAT(__utst_test_function_, test_name)();                                                              \
-    static struct UTST_PPCAT(__utst_test_register_struct_, test_name)                                                        \
-    {                                                                                                                        \
-        UTST_PPCAT(__utst_test_register_struct_, test_name)                                                                  \
-        ()                                                                                                                   \
-        {                                                                                                                    \
+#define UTST_TEST(test_name)                                                                                                    \
+    static void UTST_PPCAT(__utst_test_function_, test_name)();                                                                 \
+    static struct UTST_PPCAT(__utst_test_register_struct_, test_name)                                                           \
+    {                                                                                                                           \
+        UTST_PPCAT(__utst_test_register_struct_, test_name)                                                                     \
+        ()                                                                                                                      \
+        {                                                                                                                       \
             __utst_test_registry.register_function(UTST_STRINGIZE_NX(test_name), UTST_PPCAT(__utst_test_function_, test_name)); \
-        }                                                                                                                    \
-    } UTST_PPCAT(__utst_test_register_struct_inst_, test_name);                                                              \
+        }                                                                                                                       \
+    } UTST_PPCAT(__utst_test_register_struct_inst_, test_name);                                                                 \
     static void UTST_PPCAT(__utst_test_function_, test_name)()
 
-#define UTST_ASSERT(condition)                                                                                                                                      \
-    {                                                                                                                                                               \
-        if (!(condition))                                                                                                                                           \
-        {                                                                                                                                                           \
-            throw std::runtime_error(std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__)); \
-        }                                                                                                                                                           \
+#define UTST_ASSERT(condition)                                                                                                                              \
+    {                                                                                                                                                       \
+        if (!(condition))                                                                                                                                   \
+        {                                                                                                                                                   \
+            std::string msg = std::string("\nASSERT ") + UTST_STRINGIZE(condition) + "\n" +                                                                 \
+                              std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__); \
+            throw std::runtime_error(msg);                                                                                                                  \
+        }                                                                                                                                                   \
     }
 
-#define UTST_ASSERT_EQUAL(x, y)                                                                                                                                                                                                                   \
-    {                                                                                                                                                                                                                                             \
-        if ((x) != (y))                                                                                                                                                                                                                           \
-        {                                                                                                                                                                                                                                         \
-            std::stringstream x_sstream;                                                                                                                                                                                                          \
-            x_sstream << (x);                                                                                                                                                                                                                     \
-            std::stringstream y_sstream;                                                                                                                                                                                                          \
-            y_sstream << (y);                                                                                                                                                                                                                     \
-            throw std::runtime_error(std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__) + std::string(": ") + x_sstream.str() + std::string(" != ") + y_sstream.str()); \
-        }                                                                                                                                                                                                                                         \
+#define UTST_ASSERT_EQUAL(x, y)                                                                                                                             \
+    {                                                                                                                                                       \
+        if ((x) != (y))                                                                                                                                     \
+        {                                                                                                                                                   \
+            std::stringstream x_sstream;                                                                                                                    \
+            x_sstream << (x);                                                                                                                               \
+            std::stringstream y_sstream;                                                                                                                    \
+            y_sstream << (y);                                                                                                                               \
+            std::string msg = std::string("\nASSERT") + x_sstream.str() + std::string(" != ") + y_sstream.str() + "\n" +                                    \
+                              std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__); \
+            throw std::runtime_error(msg);                                                                                                                  \
+        }                                                                                                                                                   \
     }
