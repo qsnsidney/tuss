3438b06dfbaabd19391a4a32b5cf8b98a9f1992a
lichen-liu <issac.liu@mail.utoronto.ca>
2021-11-22 22:40:50 -0500

    Add -Wno-missing-braces

--- src/core/tests/physics_tests.cpp
+++ src/core/tests/physics_tests.cpp
@@ -7,21 +7,21 @@ UTST_MAIN();
 
 UTST_TEST(vel_updated)
 {
-    VEL v{{1.0, 2.0, 3.0}};
-    ACC a{{2.0, 4.0, 6.0}};
+    VEL v{1.0, 2.0, 3.0};
+    ACC a{2.0, 4.0, 6.0};
     VEL v_new = VEL::updated(v, a, 2.0);
 
-    VEL v_new_expected{{3.0, 6.0, 9.0}};
+    VEL v_new_expected{3.0, 6.0, 9.0};
     UTST_ASSERT_EQUAL(v_new_expected, v_new);
 }
 
 UTST_TEST(pos_updated)
 {
-    POS p{{10.0, 11.0, 12.0}};
-    VEL v{{1.0, 2.0, 3.0}};
-    ACC a{{2.0, 4.0, 6.0}};
+    POS p{10.0, 11.0, 12.0};
+    VEL v{1.0, 2.0, 3.0};
+    ACC a{2.0, 4.0, 6.0};
     POS p_new = POS::updated(p, v, a, 2.0);
 
-    POS p_new_expected{{16.0, 23.0, 30.0}};
+    POS p_new_expected{16.0, 23.0, 30.0};
     UTST_ASSERT_EQUAL(p_new_expected, p_new);
 }
--- src/cpusim/engine.h
+++ src/cpusim/engine.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "physics.h"
-
 namespace CPUSIM
 {
     /// Interface
