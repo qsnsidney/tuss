55ed9f54b99830e07234d3b63d9d3f5f2b36863e
lichen-liu <issac.liu@mail.utoronto.ca>
2021-11-22 21:31:37 -0500

    ACC::from_gravity

--- src/core/physics.cpp
+++ src/core/physics.cpp
@@ -1,14 +1,24 @@
 #include "physics.h"
+#include <cmath>
 
 namespace CORE
 {
-    VEL VEL::updated(VEL v, ACC a, DT dt)
+    ACC ACC::from_gravity(const POS &p_src, MASS m_src, const POS &p_target)
     {
-        return {v + static_cast<XYZ::value_type>(0.5) * a * dt};
+        /// TODO: utst
+        const XYZ displacement = p_target - p_src;
+        const UNIVERSE::floating_value_type denom = std::pow(displacement.norm_square() + UNIVERSE::epislon_square, -1.5f);
+
+        return {m_src * displacement * denom};
     }
 
-    POS POS::updated(POS p, VEL v, ACC a, DT dt)
+    VEL VEL::updated(const VEL &v, const ACC &a, DT dt)
     {
-        return {p + v * dt + static_cast<XYZ::value_type>(0.5) * a * dt * dt};
+        return {v + static_cast<UNIVERSE::floating_value_type>(0.5) * a * dt};
+    }
+
+    POS POS::updated(const POS &p, const VEL &v, const ACC &a, DT dt)
+    {
+        return {p + v * dt + static_cast<UNIVERSE::floating_value_type>(0.5) * a * dt * dt};
     }
 }
--- src/core/physics.h
+++ src/core/physics.h
@@ -10,17 +10,22 @@ namespace CORE
 
     using MASS = UNIVERSE::floating_value_type;
 
+    struct ACC;
+    struct VEL;
+    struct POS;
+
     struct ACC : public XYZ
     {
+        static ACC from_gravity(const POS &p_src, MASS m_src, const POS &p_target);
     };
 
     struct VEL : public XYZ
     {
-        static VEL updated(VEL, ACC, DT);
+        static VEL updated(const VEL &, const ACC &, DT);
     };
     struct POS : public XYZ
     {
-        static POS updated(POS, VEL, ACC, DT);
+        static POS updated(const POS &, const VEL &, const ACC &, DT);
     };
 
     using BODY_IC = std::tuple<POS, VEL, MASS>;
