5b019e1f00a55a0132a4df0309c42c395fd17d94
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-11-24 21:18:13 -0500

    Copy code in main into Tus SIMPLE_ENGINE (#18)
    * copy tus main into simple_engine
    
    * rename tus simple_engine into .cu
    
    * fix include simple_engine.cuh in tus
    
    * Fix missing macros in tus
    
    * hot fix
    
    * hot fix
    
    * hot fix
    
    * add inline

---
+++ src/tus/simple_engine.cuh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "core/engine.h"
+
+namespace TUS
+{
+    class SIMPLE_ENGINE final : public CORE::ENGINE
+    {
+    public:
+        virtual ~SIMPLE_ENGINE() = default;
+
+        SIMPLE_ENGINE(CORE::BODY_STATE_VEC body_states_ic,
+                      CORE::DT dt,
+                      int n_threads,
+                      std::optional<std::string> body_states_log_dir_opt = {});
+
+        virtual CORE::BODY_STATE_VEC execute(int n_iter) override;
+
+    private:
+        int n_threads_;
+    };
+}
