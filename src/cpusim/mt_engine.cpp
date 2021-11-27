63d53a970f3d22b3365437f835af2d476495b0f2
lichen-liu <issac.liu@mail.utoronto.ca>
2021-11-27 04:40:06 -0500

    cpisim threading.hpp

--- src/cpusim/mt_engine.cpp
+++ src/cpusim/mt_engine.cpp
@@ -1,48 +1,9 @@
 #include "mt_engine.h"
 #include "core/timer.h"
-
-#include <type_traits>
-#include <iostream>
-#include <thread>
 #include "buffer.h"
+#include "threading.hpp"
 
-namespace
-{
-    // Function signature: void(int i)
-    template <typename Function>
-    void parallel_for(int n_thread, int begin, int end, Function &&f)
-    {
-        typename std::decay_t<Function> f_copy = std::forward<Function>(f);
-        auto worker_thread = [&f_copy](int i_begin, int i_end)
-        {
-            for (int i = i_begin; i < i_end; i++)
-            {
-                f_copy(i);
-            }
-        };
-
-        int count = end - begin;
-        int count_per_thread = (count - 1) / n_thread + 1;
-
-        std::vector<std::thread> threads;
-        threads.reserve(n_thread);
-
-        int count_remaining = count;
-        for (int thread_idx = 0; thread_idx < n_thread; thread_idx++)
-        {
-            int i_begin = thread_idx * count_per_thread;
-            int i_size = std::min(count_per_thread, count_remaining);
-            int i_end = i_begin + i_size;
-            count_remaining -= i_size;
-            threads.emplace_back(worker_thread, i_begin, i_end);
-        }
-
-        for (auto &thread : threads)
-        {
-            thread.join();
-        }
-    }
-}
+#include <iostream>
 
 namespace CPUSIM
 {
