6bfb89bc19e09b24e90dcc6b5ee2e84ebad095e0
lichen-liu <issac.liu@mail.utoronto.ca>
2021-11-28 18:36:23 -0500

    Optimize parallel_for logics

--- src/cpusim/threading.h
+++ src/cpusim/threading.h
@@ -147,17 +147,13 @@ namespace CPUSIM
         int count = end - begin;
         int count_per_thread = (count - 1) / n_thread + 1;
 
+        // Launch
         std::vector<std::thread> threads;
         threads.reserve(n_thread);
-
-        // Launch
-        int count_remaining = count;
         for (int thread_idx = 0; thread_idx < n_thread; thread_idx++)
         {
             int i_begin = thread_idx * count_per_thread;
-            int i_size = std::min(count_per_thread, count_remaining);
-            int i_end = i_begin + i_size;
-            count_remaining -= i_size;
+            int i_end = (thread_idx == n_thread - 1) ? end : (i_begin + count_per_thread);
             threads.emplace_back(worker_thread, i_begin, i_end);
         }
 
