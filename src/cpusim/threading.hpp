e8a0096fc9c293f5017556c5b737ac2a21f8b1bb
lichen-liu <issac.liu@mail.utoronto.ca>
2021-11-28 06:37:33 -0500

    lite channel with blocking receive

--- src/cpusim/threading.hpp
+++ src/cpusim/threading.hpp
@@ -1,5 +1,8 @@
 #include <thread>
 #include <type_traits>
+#include <optional>
+#include <mutex>
+#include <condition_variable>
 
 namespace CPUSIM
 {
@@ -37,4 +40,55 @@ namespace CPUSIM
             thread.join();
         }
     }
+
+    /// A channel with capacity 1 that supports backpressure.
+    /// 1. The channel has a capacity of 1, i.e., no buffering.
+    /// 2. A send call fulfills the channel, and holds the content until
+    ///    a receive call happens.
+    ///    The try_send call is non-blocking:
+    ///    If the channel is empty, it populates the channel right away;
+    ///    If the channel is full, it immediately fails.
+    /// 3. A receive call clears out the channel.
+    ///    The receive call is blocking:
+    ///    If the channel is full, it clears the channel right away;
+    ///    If the channel is empty, it blocks and waits until the channel is full.
+    /// 4. A single send call pairs with a single receive call.
+    /// std::condition_variable: https://en.cppreference.com/w/cpp/thread/condition_variable
+    template <typename T>
+    class CHANNEL_LITE
+    {
+    public:
+        // False if the channel is already full; True if successfully sent
+        bool try_send(T data)
+        {
+            {
+                std::lock_guard<std::mutex> lock_guard(mutex_);
+                if (parcel_.has_value())
+                {
+                    return false;
+                }
+                else
+                {
+                    parcel_.emplace(std::move(data));
+                }
+            }
+            cv_has_parcel_.notify_one();
+            return true;
+        }
+
+        T receive()
+        {
+            std::unique_lock<std::mutex> unique_lock(mutex_);
+            cv_has_parcel_.wait(unique_lock, [this]()
+                                { return parcel_.has_value(); });
+            T data = std::move(parcel_.value());
+            parcel_.reset();
+            return data;
+        }
+
+    private:
+        std::optional<T> parcel_;
+        std::mutex mutex_;
+        std::condition_variable cv_has_parcel_;
+    };
 }
