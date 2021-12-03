2541d50ff548ec9d84f05bdec50bb9a44c2721cd
lichen-liu <issac.liu@mail.utoronto.ca>
2021-12-02 22:33:51 -0500

    dead code removal

--- src/tus/data_t.cuh
+++ src/tus/data_t.cuh
@@ -1,5 +1,5 @@
 #pragma once
-#include "constant.h"
+
 /*
  * Data type related helper function
  */
@@ -37,30 +37,30 @@ __host__ __device__ inline data_t_3d operator/(const data_t_3d &a, const data_t 
     return make_data_t_3d(a.x / b, a.y / b, a.z / b);
 }
 
-__host__ inline data_t gen_random_data_t(unsigned upper_bound)
-{
-    return (data_t)rand() / (data_t)(RAND_MAX / (2 * upper_bound)) - (data_t)upper_bound;
-}
+// __host__ inline data_t gen_random_data_t(unsigned upper_bound)
+// {
+//     return (data_t)rand() / (data_t)(RAND_MAX / (2 * upper_bound)) - (data_t)upper_bound;
+// }
 
-// randomly initialize the input array with type data_t in [-range, range)
-__host__ inline void random_initialize_vector(data_t_3d *input_array, size_t size, data_t range)
-{
-    for (size_t i = 0; i < size; i++)
-    {
-        input_array[i] = make_data_t_3d(gen_random_data_t(range), gen_random_data_t(range), gen_random_data_t(range));
-        //input_array[i] = make_data_t_3d(i, 2 * i, 3 * i);
-    }
-}
+// // randomly initialize the input array with type data_t in [-range, range)
+// __host__ inline void random_initialize_vector(data_t_3d *input_array, size_t size, data_t range)
+// {
+//     for (size_t i = 0; i < size; i++)
+//     {
+//         input_array[i] = make_data_t_3d(gen_random_data_t(range), gen_random_data_t(range), gen_random_data_t(range));
+//         //input_array[i] = make_data_t_3d(i, 2 * i, 3 * i);
+//     }
+// }
 
-__host__ inline void random_initialize_mass(data_t *input_array, size_t size, data_t range)
-{
-    //hack: + range at the end to offset the nagative
-    for (size_t i = 0; i < size; i++)
-    {
-        input_array[i] = gen_random_data_t(range) + range;
-        //input_array[i] = (float)(i) / 2;
-    }
-}
+// __host__ inline void random_initialize_mass(data_t *input_array, size_t size, data_t range)
+// {
+//     //hack: + range at the end to offset the nagative
+//     for (size_t i = 0; i < size; i++)
+//     {
+//         input_array[i] = gen_random_data_t(range) + range;
+//         //input_array[i] = (float)(i) / 2;
+//     }
+// }
 
 // WARNING: this function has hardcoded assumption on float vs double
 __device__ inline data_t power_norm(data_t_3d a, data_t_3d b)
