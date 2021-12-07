86fd80726920fc0867a5d7b5662e3cafe5d11103
lichen-liu <issac.liu@mail.utoronto.ca>
2021-12-06 20:08:02 -0500

    play around with yt project

--- scripts/bicgen/__main__.py
+++ scripts/bicgen/__main__.py
@@ -1,5 +1,8 @@
 from . import translation
 
+# in_tipsy_file_path = './data/tipsy/LOW/LOW.bin'
+in_tipsy_file_path = None
+print('Info:', 'Loading from', in_tipsy_file_path)
 translation.from_tipsy_into_bin(
-    in_tipsy_file_path=None, out_bin_file_path=None)
+    in_tipsy_file_path=in_tipsy_file_path, out_bin_file_path=None)
 print('hello')
--- scripts/bicgen/translation.py
+++ scripts/bicgen/translation.py
@@ -2,6 +2,9 @@ try:
     import yt
 except Exception as e:
     print('Try `pip3 install yt`')
+    print('Try `pip3 install pooch`')
+    print('Try `pip3 install pandas`')
+
 from .. import core
 
 # https://yt-project.org/doc/cookbook/tipsy_notebook.html
@@ -12,5 +15,17 @@ def from_tipsy_into_bin(in_tipsy_file_path, out_bin_file_path):
     core.serde.serialize_system_state_into_bin(system_state, out_bin_file_path)
 
 
-def deserialize_system_state_from_tipsy(tipsy_file_path):
-    pass
+def deserialize_system_state_from_tipsy(tipsy_file_path=None):
+    '''
+    Downloads and uses a sample from yt project if tipsy_file_path=None
+    '''
+    if tipsy_file_path is None:
+        ds = yt.load_sample('TipsyGalaxy')
+    else:
+        ds = yt.load(tipsy_file_path)
+    print(ds.field_list)
+
+    print(ds.particle_types)
+    print(ds.particle_types_raw)
+    print(ds.particle_type_counts)
+    return None
