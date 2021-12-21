3313b8b98f98c22d5ec9fcdb6ddc550ade88907a
lichen-liu <issac.liu@mail.utoronto.ca>
2021-12-21 18:45:20 -0500

    merge coalesced kernel into engine

--- src/tus/coalesced_simple_engine.cu
+++ src/tus/coalesced_simple_engine.cu
@@ -12,7 +12,66 @@
 #include "core/serde.h"
 #include "helper.cuh"
 #include "data_t.cuh"
-#include "coalesced_engine_kernel.cuh"
+
+__global__ inline void update_step_pos_f4(unsigned nbody, data_t step_size, float4 *i_location, data_t_3d *i_velocity, float4 *i_accer, // new accer is accer at i+1 iteration
+                                          float4 *o_location, data_t_3d *velocity_half)
+{
+    unsigned tid = threadIdx.x + blockDim.x * blockIdx.x;
+    if (tid < nbody)
+    {
+        // v1/2          =         vi      +     ai  *          1/2 *    dt
+        velocity_half[tid].x = i_velocity[tid].x + i_accer[tid].x * ((data_t)0.5 * step_size);
+        velocity_half[tid].y = i_velocity[tid].y + i_accer[tid].y * ((data_t)0.5 * step_size);
+        velocity_half[tid].z = i_velocity[tid].z + i_accer[tid].z * ((data_t)0.5 * step_size);
+        // Xi+1         =      xi         +       vi        *     dt    +    ai   *     1/2     *     (dt)^2
+        o_location[tid].x = i_location[tid].x + i_velocity[tid].x * step_size + i_accer[tid].x * (data_t)0.5 * powf(step_size, 2);
+        o_location[tid].y = i_location[tid].y + i_velocity[tid].y * step_size + i_accer[tid].y * (data_t)0.5 * powf(step_size, 2);
+        o_location[tid].z = i_location[tid].z + i_velocity[tid].z * step_size + i_accer[tid].z * (data_t)0.5 * powf(step_size, 2);
+        o_location[tid].w = i_location[tid].w;
+
+        //printf("tid = %d, half_v %f, %f, %f\no_location %f, %f, %f\n", tid, velocity_half[tid].x, velocity_half[tid].y, velocity_half[tid].z, o_location[tid].x, o_location[tid].y, o_location[tid].z);
+    }
+}
+
+__global__ inline void update_step_vel_f4(unsigned nbody, data_t step_size, float4 *new_accer, data_t_3d *velocity_half, // new accer is accer at i+1 iteration
+                                          data_t_3d *o_velocity)
+{
+    unsigned tid = threadIdx.x + blockDim.x * blockIdx.x;
+    if (tid < nbody)
+    {
+        o_velocity[tid].x = velocity_half[tid].x + new_accer[tid].x * ((data_t)0.5 * step_size);
+        o_velocity[tid].y = velocity_half[tid].y + new_accer[tid].y * ((data_t)0.5 * step_size);
+        o_velocity[tid].z = velocity_half[tid].z + new_accer[tid].z * ((data_t)0.5 * step_size);
+        //printf("tid = %d, update_v %f, %f, %f\n", tid, o_velocity[tid].x, o_velocity[tid].y, o_velocity[tid].z);
+    }
+}
+
+__global__ inline void calculate_acceleration_f4(unsigned nbody, float4 *location, float4 *acceleration)
+{
+    unsigned tid = threadIdx.x + blockDim.x * blockIdx.x;
+    if (tid < nbody)
+    {
+        data_t_3d accumulated_accer = make_data_t_3d(0, 0, 0);
+        data_t_3d x_self = make_data_t_3d(location[tid].x, location[tid].y, location[tid].z);
+        for (unsigned j = 0; j < nbody; j++)
+        {
+            if (j == tid)
+            {
+                continue;
+            }
+            // source of gravitiy
+            data_t_3d x_source = make_float3(location[j].x, location[j].y, location[j].z);
+            data_t mass = location[j].w;
+
+            data_t_3d numerator = (x_source - x_self) * mass;
+            data_t denominator = power_norm(x_self, x_source);
+            data_t_3d new_term = (numerator / denominator);
+            accumulated_accer = accumulated_accer + new_term;
+            //printf("tid = %d, new_term %f, %f, %f\n", tid, new_term.x, new_term.y, new_term.z);
+        }
+        acceleration[tid] = make_float4(accumulated_accer.x, accumulated_accer.y, accumulated_accer.z, 0);
+    }
+}
 
 namespace
 {
@@ -35,9 +94,8 @@ namespace TUS
     COALESCED_SIMPLE_ENGINE::COALESCED_SIMPLE_ENGINE(CORE::SYSTEM_STATE system_state_ic,
                                                      CORE::DT dt,
                                                      int block_size,
-                                                     std::optional<std::string> system_state_log_dir_opt) : 
-                                                        ENGINE(std::move(system_state_ic), dt, std::move(system_state_log_dir_opt)),
-                                                        block_size_(block_size)
+                                                     std::optional<std::string> system_state_log_dir_opt) : ENGINE(std::move(system_state_ic), dt, std::move(system_state_log_dir_opt)),
+                                                                                                            block_size_(block_size)
     {
     }
 
@@ -117,17 +175,17 @@ namespace TUS
             for (int i_iter = 0; i_iter < n_iter; i_iter++)
             {
                 update_step_pos_f4<<<nblocks, block_size_>>>(nBody, (data_t)dt(), d_X[src_index], d_V[src_index], d_A[src_index], //input
-                                                            d_X[dest_index], d_V_half);                                               // output
+                                                               d_X[dest_index], d_V_half);                                          // output
 
                 cudaDeviceSynchronize();
 
                 calculate_acceleration_f4<<<nblocks, block_size_>>>(nBody, d_X[dest_index], //input
-                                                                   d_A[dest_index]);            // output
+                                                                      d_A[dest_index]);       // output
 
                 cudaDeviceSynchronize();
 
                 update_step_vel_f4<<<nblocks, block_size_>>>(nBody, (data_t)dt(), d_A[dest_index], d_V_half, //input
-                                                            d_V[dest_index]);                                    // output
+                                                               d_V[dest_index]);                               // output
                 cudaDeviceSynchronize();
 
                 timer.elapsed_previous(std::string("iter") + std::to_string(i_iter), CORE::TIMER::TRIGGER_LEVEL::INFO);
@@ -181,7 +239,8 @@ namespace TUS
         cudaFreeHost(h_output_X);
         cudaFreeHost(h_output_V);
 
-        for(const auto i : {src_index, dest_index}){
+        for (const auto i : {src_index, dest_index})
+        {
             cudaFree(d_X[i]);
             cudaFree(d_V[i]);
             cudaFree(d_A[i]);
