0c6392144fb9f89d45a5035daecbed4bacfb775e
Sining <qsnsidney@gmail.com>
2021-12-26 15:53:40 +0800

    debug

--- src/tus/small_N_engine.cu
+++ src/tus/small_N_engine.cu
@@ -235,7 +235,7 @@ __global__ void reduce(float4 *g_idata, float4 *g_odata, int ilen, int olen, int
                 if (tid == 0) 
                 {
                     //g_odata[vo + olen*j] = sdata[0];
-                    printf("%d block %d has data %f\n", j, blockIdx.x, sdata[0]);
+                    printf("%d block %d has data\n", j, blockIdx.x;
                     if (blkn == 1)
                     {
                         //o[blockIdx.y*bn+j] = sdata[0];
