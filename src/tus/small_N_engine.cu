f66182f8433c2ed6666f413a071370cf2d956963
Sining <qsnsidney@gmail.com>
2021-12-26 17:53:12 +0800

    debug

--- src/tus/small_N_engine.cu
+++ src/tus/small_N_engine.cu
@@ -265,7 +265,7 @@ __global__ void reduce(float4 *g_idata, float4 *g_odata, int ilen, int olen, int
 
                 if (tid == 0 && blkn == 1) 
                 {
-                    printf("%d block (%d, %d) output x: %f, y: %f, z: %f\n", blockIdx.y*ilen*bn + ilen*j + i, blockIdx.x, blockIdx.y, o[blockIdx.y*bn+j].x, o[blockIdx.y*bn+j].y, o[blockIdx.y*bn+j].z);
+                    printf("%d block (%d, %d) output x: %f, y: %f, z: %f\n", blockIdx.y*bn + j, blockIdx.x, blockIdx.y, o[blockIdx.y*bn+j].x, o[blockIdx.y*bn+j].y, o[blockIdx.y*bn+j].z);
                 }
 
             }
